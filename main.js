a626cd33200b1931b67836ba13bdc7749a720078
Yiran Wang (Elaine) <90149606+ywanglf@users.noreply.github.com>
2023-12-13 22:20:31 -0500

    Setup

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings:["Graduate Student", "Wannabe Programmer", "Gamer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
